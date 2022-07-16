60af09d4bea9d2dfd5f382c2667bd15e22b12041
guanmufu <854342641@qq.com>
2022-07-16 23:03:16 +0800

    Push Code:2022/07/16,23:03:16

--- p.bat
+++ p.bat
@@ -1,7 +1,6 @@
-E:
-cd E:\WorkQD\HBGit\hbdist\hei25hei.github.io
-git init 
+﻿G:
+cd G:\guanmufu\guanmufu.github.io
 git add . 
-git commit -m "Push Code:%date:~0,10%,%time:~0,8%" （<-关注点在这里）
+git commit -m "Push Code:%date:~0,10%,%time:~0,8%"
 git push origin
 pause>nul
